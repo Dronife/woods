eff638171519a9b551605241d41a5e8a0e5304f2
Dronife <d.smeledis@gmail.com>
2021-02-26 16:19:16 +0200

    Created new branch so I could rewrite the website to clean code

--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,50 @@ Route::get('/about', function () {
 
 
 
+
+
+// ***** Account control *****
+Route::get('/account', [backendController::class, 'getAccount']);
 Route::post('/update-general-account', [backendController::class, 'updateGeneralAccount']);
 Route::post('/update-password-account', [backendController::class, 'updatePasswordAccount']);
-Route::get('/account', [backendController::class, 'getAccount']);
-Route::get('/userpanel', [backendController::class, 'userAdmin']);
-Route::get('/create-forest', [backendController::class, 'createForestConf']);
-Route::post('/submit-forest',[backendController::class, 'addForestConf']);
-Route::post('/add-pictures/{id}/{redirect}',[backendController::class, 'addPictures']);
-Route::get('/slide-show/{id}', [backendController::class, 'getPictures']);
-Route::post('/identificationNumber/{id}', [backendController::class, 'forestIDnum']);
-Route::post('/configCreateDefaults', [backendController::class, 'configCreateDefaults']);
 
-Route::delete('/deleteSubmittion/{id}', [backendController::class, 'deleteSubmitedForest']);
-Route::delete('/deletepic/{id}', [backendController::class, 'deletePicture']);
+// ***** User control *****
 Route::delete('/destroyUser/{id}', [backendController::class, 'deleteUser']);
-Route::get('/getsubmitedForest/{id}', [backendController::class, 'getSubmitedForest']);
-Route::get('/configuration', [backendController::class, 'config']);
-
-Route::post('/updatesubmitedForest', [backendController::class, 'updateSubmitedForest']);
-
 Route::get('/users', [backendController::class, 'getUsers']);
-Route::post('/submit-user-list', [backendController::class, 'submitUserList']);
 Route::post('/admin-register-submit', [backendController::class, 'adminRegister']);
 
+
+
+// ***** Configuration *****
+
+Route::post('/submit-user-list', [backendController::class, 'submitUserList']);
+Route::post('/configCreateDefaults', [backendController::class, 'configCreateDefaults']);
+Route::get('/configuration', [backendController::class, 'config']);
+
+
+// ***** Forest forests*****
+
+Route::get('/create-forest', [backendController::class, 'createForestConf']);
+Route::post('/submit-forest',[backendController::class, 'addForestConf']);
+Route::post('/identificationNumber/{id}', [backendController::class, 'forestIDnum']);
+Route::delete('/deleteSubmittion/{id}', [backendController::class, 'deleteSubmitedForest']);
+Route::get('/getsubmitedForest/{id}', [backendController::class, 'getSubmitedForest']);
+Route::post('/updatesubmitedForest', [backendController::class, 'updateSubmitedForest']);
+
+
+// ***** View Pictures *****
+Route::post('/add-pictures/{id}/{redirect}',[backendController::class, 'addPictures']);
+Route::get('/slide-show/{id}', [backendController::class, 'getPictures']);
+Route::delete('/deletepic/{id}', [backendController::class, 'deletePicture']);
+
+
+
+
+
+
+
+
 Auth::routes();
+Route::get('/userpanel', [backendController::class, 'userAdmin']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
